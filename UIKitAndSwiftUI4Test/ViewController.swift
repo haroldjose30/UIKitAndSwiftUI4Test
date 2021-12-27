ad3be78850aceaa683de90b81a9dca3983bc0ea8
EXTERNAL Harold Jose (Olisipo, TT-RHC/PAR-Av) <external.Jose.Harold@pt.bosch.com>
2021-12-27 18:21:24 -0300

    initial commit

---
+++ UIKitAndSwiftUI4Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitAndSwiftUI4Test
+//
+//  Created by Jose Harold on 21/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
